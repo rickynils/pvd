a299cb8aa842c0110bb7c2fb0efa96dd5b156817
Rickard Nilsson <rickynils@gmail.com>
2009-12-29 13:09:01 +0100

    Fixed DevIL initialisation
    Ignore-this: c25861d7c3c8686d9a6b9968672d38bc
    
    darcs-hash:20091229120901-63515-6a5ad8ff55de1f858b17a600416800f8a7574132

--- Main.hs
+++ Main.hs
@@ -1,12 +1,18 @@
-module Main where
+module Main (
+  main
+) where
+
+import Codec.Image.DevIL
+import Control.Concurrent (threadDelay)
+import Data.Array.IArray
+import Data.Array.Unboxed
+import Data.Array.MArray
+import Data.Array.Storable
 import Data.Bits
+import Foreign.C.Types
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras
 import System.Exit (exitWith, ExitCode(..))
-import Control.Concurrent (threadDelay)
-import Data.Array.Storable
-import Data.Array.MArray
-import Foreign.C.Types
 
 main :: IO ()
 main = do
@@ -15,24 +21,26 @@ main = do
   win <- mkWin dpy rootw
   selectInput dpy win (exposureMask .|. buttonPressMask)
   mapWindow dpy win
-  updateWin dpy win
+  ilInit
+  imgData <- readImage "/srv/photo/wrk/export/agneta_korkort.jpg"
+  putStrLn $ show (bounds imgData)
+  updateWin dpy win imgData
+  return ()
 
-updateWin :: Display -> Window -> IO ()
-updateWin dpy win = do
-  drawInWin dpy win
+updateWin dpy win imgData = do
+  drawInWin dpy win imgData
   sync dpy True
   allocaXEvent $ \e -> do
     nextEvent dpy e
     ev <- getEvent e
     putStrLn $ eventName ev
-    if ev_event_type ev /= buttonPress then updateWin dpy win else return ()
+    if ev_event_type ev /= buttonPress then updateWin dpy win imgData else return ()
 
 mkWin dpy rootw = do
   col <- initColor dpy "#444444"
   createSimpleWindow dpy rootw 0 0 100 100 1 col col
 
-drawInWin :: Display -> Window -> IO ()
-drawInWin dpy win = do
+drawInWin dpy win imgData = do
   bgcolor <- initColor dpy "#444444"
   gc <- createGC dpy win
   (_,_,_,w,h,_,_) <- getGeometry dpy win
@@ -43,7 +51,7 @@ drawInWin dpy win = do
   p <- createPixmap dpy win w h depth
   setForeground dpy gc bgcolor
   fillRectangle dpy p gc 0 0 w h
-  img <- mkImg dpy vis iw ih depth
+  img <- mkImg dpy vis iw ih depth imgData
   putImage dpy p gc img 0 0 (fromIntegral $ (w-iw) `div` 2) (fromIntegral $ (h-ih) `div` 2) iw ih
   copyArea dpy p win gc 0 0 w h 0 0
   freeGC dpy gc
@@ -55,10 +63,13 @@ initColor dpy color = do
   (apros,real) <- allocNamedColor dpy colormap color
   return $ color_pixel apros
 
-mkImg d v w h depth = do
-  let bounds = (0, (4 * (fromIntegral h) * (fromIntegral w)) - 1)
-  --let bounds = (0, 3 * (fromIntegral h-1) * (fromIntegral w-1))
-  ar <- newArray bounds 50 :: IO (StorableArray Int CChar)
+mkImg :: Display -> Visual -> Dimension -> Dimension -> CInt -> UArray (Int,Int,Int) Word8 -> IO Image
+mkImg dpy vis w h depth imgData = do
+  --let bs = (0, 3 * (fromIntegral h-1) * (fromIntegral w-1))
+  ar <- newArray bs 50 :: IO (StorableArray Int CChar)
+  ar <- newListArray bs ls :: IO (StorableArray Int CChar)
+
+
   writeArray ar 0 255
   writeArray ar 1 255
   writeArray ar 2 255
@@ -70,4 +81,13 @@ mkImg d v w h depth = do
   writeArray ar (4*10*(fromIntegral w) + 4*10) 255
   --writeArray ar (4) 255
   withStorableArray ar $ \ptr ->
-    createImage d v depth zPixmap 0 ptr w h 32 0
+    createImage dpy vis depth zPixmap 0 ptr w h 32 0
+
+    where
+      bs = (0, (4 * (fromIntegral h) * (fromIntegral w)) - 1)
+      f :: Int -> (Int,Int,Int)
+      f n = (0,0,0)
+      ar :: UArray Int Word8
+      ar = ixmap bs f imgData
+      ls :: [CChar]
+      ls = map fromIntegral $ elems ar
