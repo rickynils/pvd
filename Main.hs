b186dff23c9b92dd0ef2fd98ba486c3786ca44d2
Rickard Nilsson <rickynils@gmail.com>
2010-01-07 21:19:29 +0100

    Cleaned up code, changed image image loading, implemented very basic scrolling
    Ignore-this: 80f69f2e6efcae632256f1aa6dc7ce3e
    
    darcs-hash:20100107201929-63515-e4ef250ffdb0dc21d413886846bca55445f85bbf

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main (
-  main
+  main,
+  scale
 ) where
 
 import Codec.Image.DevIL
@@ -9,6 +10,7 @@ import Data.Array.Unboxed
 import Data.Array.MArray
 import Data.Array.Storable
 import Data.Bits
+import Debug.Trace
 import Foreign.C.Types
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras
@@ -19,46 +21,79 @@ main = do
   dpy <- openDisplay ""
   rootw <- rootWindow dpy (defaultScreen dpy)
   win <- mkWin dpy rootw
-  selectInput dpy win (exposureMask .|. buttonPressMask)
+  selectInput dpy win (exposureMask .|. buttonPressMask .|. keyPressMask)
   mapWindow dpy win
   ilInit
-  imgData <- readImage "/srv/photo/wrk/export/agneta_korkort.jpg"
-  putStrLn $ show (bounds imgData)
-  updateWin dpy win imgData
+  imgData <- readImage "/srv/photo/wrk/export/rhododendron.tif"
+  let depth = defaultDepthOfScreen (defaultScreenOfDisplay dpy)
+      vis = defaultVisual dpy (defaultScreen dpy)
+  img <- mkImg dpy vis depth imgData s
+  updateWin dpy win imgData img 0 0
   return ()
 
-updateWin dpy win imgData = do
-  drawInWin dpy win imgData
+updateWin dpy win imgData img x y = do
+  drawInWin dpy win imgData img x y
   sync dpy True
   allocaXEvent $ \e -> do
     nextEvent dpy e
     ev <- getEvent e
     putStrLn $ eventName ev
-    if ev_event_type ev /= buttonPress then updateWin dpy win imgData else return ()
+    handleEvent $ ev_event_type ev
+  where
+    handleEvent ev | ev == buttonPress = return ()
+                   | ev == keyPress = updateWin dpy win imgData img (x+1) y
+                   | otherwise = updateWin dpy win imgData img x y
+
+
+--redraw xo yo sc img = do
+--  let (w,h,_) = (snd . bounds) img
+--  let (w',h') = (div w 2, div h 2)
+--  let bounds' = (0, w'*h')
+--  imgArr
+
+s = (1,6)
+
 
 mkWin dpy rootw = do
   col <- initColor dpy "#444444"
   createSimpleWindow dpy rootw 0 0 100 100 1 col col
 
-drawInWin :: Display -> Window -> UArray (Int,Int,Int) Word8 -> IO ()
-drawInWin dpy win imgData = do
+drawInWin ::
+  Display ->
+  Window ->
+  UArray (Int,Int,Int) Word8 ->
+  Image ->
+  Int ->
+  Int ->
+  IO ()
+drawInWin dpy win imgData img x y = do
   bgcolor <- initColor dpy "#444444"
   gc <- createGC dpy win
-  (_,_,_,w,h,_,_) <- getGeometry dpy win
+  (_,_,_,winWidth,winHeight,_,_) <- getGeometry dpy win
   let depth = defaultDepthOfScreen (defaultScreenOfDisplay dpy)
       vis = defaultVisual dpy (defaultScreen dpy)
-      ((_,_,_),(ih,iw,_)) = bounds imgData
-      scale = 4
-      dw = fromIntegral $ min (w `div` 2) $ fromIntegral (iw `div` scale)
-      dh = fromIntegral $ min (h `div` 2) $ fromIntegral (ih `div` scale)
-  p <- createPixmap dpy win w h depth
+      (imgHeight,imgWidth,_) = snd $ bounds imgData
+      (viewWidth,viewHeight) = scale s (imgWidth,imgHeight)
+      portWidth = min (winWidth `div` 2) viewWidth
+      portHeight = min (winHeight `div` 2) viewHeight
+      portX = (winWidth-portWidth) `div` 2
+      portY = (winHeight-portHeight) `div` 2
+      viewX = fromIntegral $ min (max 0 (fromIntegral x)) (viewWidth-portWidth)
+      viewY = fromIntegral $ min (max 0 (fromIntegral y)) (viewHeight-portHeight)
+  putStrLn $ show (imgWidth,imgHeight)
+  putStrLn $ show (portWidth,portHeight)
+  putStrLn $ show (viewWidth,viewHeight)
+  putStrLn $ show (winWidth,winHeight)
+  putStrLn $ show (portX,portY)
+  putStrLn $ show (x,y)
+  putStrLn $ show (viewX,viewY)
+  pixmap <- createPixmap dpy win winWidth winHeight depth
   setForeground dpy gc bgcolor
-  fillRectangle dpy p gc 0 0 w h
-  img <- mkImg dpy vis dw dh depth imgData (fromIntegral scale)
-  putImage dpy p gc img 0 0 (fromIntegral $ (w-dw) `div` 2) (fromIntegral $ (h-dh) `div` 2) dw dh
-  copyArea dpy p win gc 0 0 w h 0 0
+  fillRectangle dpy pixmap gc 0 0 winWidth winHeight
+  putImage dpy pixmap gc img viewX viewY (fromIntegral portX) (fromIntegral portY) portWidth portHeight
+  copyArea dpy pixmap win gc 0 0 winWidth winHeight 0 0
   freeGC dpy gc
-  freePixmap dpy p
+  freePixmap dpy pixmap
 
 initColor :: Display -> String -> IO Pixel
 initColor dpy color = do
@@ -66,23 +101,40 @@ initColor dpy color = do
   (apros,real) <- allocNamedColor dpy colormap color
   return $ color_pixel apros
 
-mkImg :: Display -> Visual -> Dimension -> Dimension -> CInt -> UArray (Int,Int,Int) Word8 -> Int -> IO Image
-mkImg dpy vis w h depth imgData scale = do
-  ar <- newListArray bs ls :: IO (StorableArray Int CChar)
-  withStorableArray ar $ \ptr ->
-    createImage dpy vis depth zPixmap 0 ptr w h 32 0
+mkImg ::
+  Display ->
+  Visual ->
+  CInt ->
+  UArray (Int,Int,Int) Word8 ->
+  Scale ->
+  IO Image
+mkImg dpy vis depth imgData s = do
+  byteArr <- newListArray (0,byteCount) bytes
+  withStorableArray byteArr $ \ptr ->
+    createImage dpy vis depth zPixmap 0 ptr (fromIntegral w) (fromIntegral h) 32 0
 
     where
-      ((_,_,_),(ih,iw,_)) = bounds imgData
-      bs = (0, (4 * (fromIntegral h) * (fromIntegral w)) - 1)
-      f :: Int -> (Int,Int,Int)
-      f n = (ih-(scale*r), iw-(scale*c), l)
+      (ih,iw,_) = snd $ bounds imgData
+      (h,w) = scale s (ih,iw)
+      byteCount = (4*h*w) - 1
+      bytes = map fromIntegral $ elems $ ixmap (0,byteCount) f imgData
+      f n = (ih-sr, iw-sc, l)
         where
-          c = (n `div` 4) `mod` (fromIntegral w)
-          r = (n `div` 4) `div` (fromIntegral w)
+          (sc,sr) = scale (snd s, fst s) (c,r)
+          c = (n `div` 4) `mod` w
+          r = (n `div` 4) `div` w
           l = case n `mod` 4 of
                 0 -> 2
                 2 -> 0
                 m -> m
-      ar = ixmap bs f imgData
-      ls = map fromIntegral $ elems ar
+
+scale (s1,s2) (h,w) = (h',w')
+  where
+    h' = round $ fromIntegral (s1*h) / fromIntegral s2
+    w' = round $ fromIntegral (s1*w) / fromIntegral s2
+
+
+type Scale = (Int,Int)
+type PixelPos = (Int,Int,Int)
+type Transformer = Scale -> PixelPos -> [(PixelPos,Int)]
+
