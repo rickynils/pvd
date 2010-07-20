3749874f6e8f941a88a6043ba80c4eea84538945
Rickard Nilsson <rickynils@gmail.com>
2010-07-20 15:27:45 +0200

    Added code for parsing command line options in pvc
    Ignore-this: 60f1b01dec5b5209ca2019786b798c0f
    
    darcs-hash:20100720132745-63515-8609da4c1b00f2c30ed617f7038b70144768ffbd

--- Pvc.hs
+++ Pvc.hs
@@ -4,4 +4,63 @@ module Main (
   main
 ) where
 
-main = return ()
+import System (getArgs)
+import System.Console.GetOpt
+import Control.Monad (when)
+import qualified Data.Map as Map
+
+data Flag =
+  FileList String | Port String | Add | Insert | Replace
+
+data Command = Command {
+  cmdStr :: String,
+  cmdOptions :: [OptDescr Flag],
+  cmdAction :: [Flag] -> [String] -> IO (),
+  cmdUsage :: String
+}
+
+main = do
+  args <- getArgs
+  when (null args) (usageError Nothing "No command given")
+  (cmd, flags, files) <- parseOptions (head args) (tail args)
+  cmd flags files
+
+usageError Nothing msg = do
+  putStrLn "Usage:\n  pvc COMMAND [OPTIONS]\n"
+  putStrLn (usageInfo "Options valid for all commands:" commonOptions)
+  putStrLn "Available commands:"
+  putStrLn $ unlines $ map ("  "++) (Map.keys commandMap)
+  fail msg
+
+usageError (Just (Command cn opts _ usage)) msg = do
+  putStrLn ("Usage:\n  pvc "++cn++" "++usage++"\n")
+  putStrLn (usageInfo "Options valid for all commands:" commonOptions)
+  putStrLn (usageInfo ("Options valid for command \""++cn++"\":") opts)
+  fail msg
+
+parseOptions cmd argv = case Map.lookup cmd commandMap of
+  Nothing -> usageError Nothing $ "Unknown command: "++cmd
+  Just c -> case getOpt Permute (commonOptions++(cmdOptions c)) argv of
+    (o,n,[]  ) -> return (cmdAction c, o, n)
+    (_,_,errs) -> usageError (Just c) (concat $ map (filter ('\n' /=)) errs)
+
+commonOptions =
+  [ Option ['p'] ["port"] (ReqArg Port "PORT") "photo viewer daemon port"
+  ]
+
+commands =
+  [ Command "playlist" playlistOpts playlistAct playlistUsage
+  ]
+
+commandMap = Map.fromList $ map (\c -> (cmdStr c, c)) commands
+
+playlistOpts =
+  [ Option ['l'] ["filelist"] (ReqArg FileList "FILE") "a file that contains paths of the photos that should be added to the playlist"
+  , Option ['a'] ["add"] (NoArg Add) "adds the selected files to the end of the current playlist"
+  , Option ['r'] ["replace"] (NoArg Replace) "replaces the contents of the current playlist with the selected files"
+  , Option ['i'] ["insert"] (NoArg Insert) "inserts the selected files first in the current playlist"
+  ]
+
+playlistAct _ _ = return ()
+
+playlistUsage = "[OPTIONS] [FILES]\n\n  Manages the current pvd playlist"
